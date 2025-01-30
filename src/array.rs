57e192a441d25318014b32025ae59717c2ad65ea
Marcus Cveticanin <mjovanc@icloud.com>
2025-01-30 20:50:56 +0100

    Removing stuff temp

--- src/array.rs
+++ src/array.rs
@@ -22,46 +22,6 @@ impl<T> Array<T> {
     }
 }
 
-impl<T: Numeric> Array<T> {
-    pub fn zeros(&mut self) {
-        let len = self.data.len();
-        self.data = T::zeros(len);
-    }
-
-    pub fn zeros_with_len(&mut self, len: usize) {
-        self.data = T::zeros(len);
-    }
-
-    pub fn ones(&mut self) {
-        let len = self.data.len();
-        self.data = T::ones(len);
-    }
-
-    pub fn ones_with_len(&mut self, len: usize) {
-        self.data = T::ones(len);
-    }
-}
-
-impl Numeric for i64 {
-    fn zeros(len: usize) -> Vec<Self> {
-        vec![0; len]
-    }
-
-    fn ones(len: usize) -> Vec<Self> {
-        vec![1; len]
-    }
-}
-
-impl Numeric for f64 {
-    fn zeros(len: usize) -> Vec<Self> {
-        vec![0.0; len]
-    }
-
-    fn ones(len: usize) -> Vec<Self> {
-        vec![1.0; len]
-    }
-}
-
 impl Array<i64> {
     pub fn dtype(&self) -> &'static str {
         "int64"
@@ -102,61 +62,5 @@ mod tests {
     fn test_array_creation_empty() {
         let a: Array<i64> = array![];
         assert!(a.data().is_empty());
-    }
-
-    #[test]
-    fn test_array_i64_zeros() {
-        let mut a2 = array![1, 2, 3];
-        a2.zeros();
-        assert_eq!(a2.data(), &vec![0, 0, 0]);
-    }
-
-    #[test]
-    fn test_array_f64_zeros() {
-        let mut a = array![1.0, 2.0, 3.0];
-        a.zeros();
-        assert_eq!(a.data(), &vec![0.0, 0.0, 0.0]);
-    }
-
-    #[test]
-    fn test_array_i64_zeros_with_len() {
-        let mut a2 = array![1, 2, 3];
-        a2.zeros_with_len(5);
-        assert_eq!(a2.data(), &vec![0, 0, 0, 0, 0]);
-    }
-
-    #[test]
-    fn test_array_f64_zeros_with_len() {
-        let mut a2 = array![1.0, 2.0, 3.0];
-        a2.zeros_with_len(5);
-        assert_eq!(a2.data(), &vec![0.0, 0.0, 0.0, 0.0, 0.0]);
-    }
-
-    #[test]
-    fn test_array_i64_ones() {
-        let mut a2 = array![1, 2, 3];
-        a2.ones();
-        assert_eq!(a2.data(), &vec![1, 1, 1]);
-    }
-
-    #[test]
-    fn test_array_f64_ones() {
-        let mut a = array![1.0, 2.0, 3.0];
-        a.ones();
-        assert_eq!(a.data(), &vec![1.0, 1.0, 1.0]);
-    }
-
-    #[test]
-    fn test_array_i64_ones_with_len() {
-        let mut a2 = array![1, 2, 3];
-        a2.ones_with_len(2);
-        assert_eq!(a2.data(), &vec![1, 1]);
-    }
-
-    #[test]
-    fn test_array_f64_ones_with_len() {
-        let mut a2 = array![1.0, 2.0, 3.0];
-        a2.ones_with_len(2);
-        assert_eq!(a2.data(), &vec![1.0, 1.0]);
     }
 }
