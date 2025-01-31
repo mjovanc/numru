39f8e560424a13dcedfbc765695fa6094fb5a6c5
Marcus Cveticanin <mjovanc@icloud.com>
2025-01-31 16:17:04 +0100

    Updating example

--- examples/example.rs
+++ examples/example.rs
@@ -2,19 +2,26 @@ use numru::array;
 use std::f64::consts::{PI, TAU, E};
 
 fn main() {
+    println!("#1 example:");
     let a = array![42, -17, 256, 3, 99, -8];
-    println!("{:?}", a);
+    println!("a={:?}", a);
+    println!("DataType={:?}", a.dtype());
+    println!("Shape={:?}", a.shape());
 
+    println!("\n#2 example:");
     let b = array![
         [TAU, -PI, 1.61],
         [E,  0.98, -7.42],
         [4.67, -0.45, 8.88],
     ];
-    println!("{:?}", b);
+    println!("b={:?}", b);
+    println!("Shape={:?}", b.shape());
 
+    println!("\n#3 example:");
     let c = array![
         [[101, 202, 303], [404, 505, 606]],
         [[-707, -808, -909], [111, 222, 333]]
     ];
-    println!("{:?}", c);
+    println!("c={:?}", c);
+    println!("Shape={:?}", c.shape());
 }
