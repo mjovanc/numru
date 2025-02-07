074efc1e242099395916adb5a171b8ad8ca1e273
Marcus Cveticanin <mjovanc@icloud.com>
2025-02-07 13:10:23 +0100

    Updating unit tests

--- src/array.rs
+++ src/array.rs
@@ -25,55 +25,60 @@ impl<D: Dimension> Array<i64, D> {
     pub fn dtype(&self) -> &'static str {
         "int64"
     }
+
+    pub fn mean(&self, axis: Option<usize>) -> Vec<i64> {
+        !unimplemented!()
+    }
+
+    pub fn min(&self, axis: Option<usize>) -> i64 {
+        !unimplemented!()
+    }
+
+    pub fn max(&self, axis: Option<usize>) -> i64 {
+        !unimplemented!()
+    }
 }
 
 impl<D: Dimension> Array<f64, D> {
     pub fn dtype(&self) -> &'static str {
         "float64"
     }
+
+    pub fn mean(&self, axis: Option<usize>) -> Vec<f64> {
+        !unimplemented!()
+    }
+
+    pub fn min(&self, axis: Option<usize>) -> f64 {
+        !unimplemented!()
+    }
+
+    pub fn max(&self, axis: Option<usize>) -> f64 {
+        !unimplemented!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::shape::{Ix, Shape, Dimension};
-    use crate::Array;
 
     #[test]
     fn test_array_creation_1d() {
-        let data = vec![1, 2, 3, 4]; // TODO: change to arr![] later
+        let data = arr![1, 2, 3, 4];
         let shape = Shape::new(Ix::<1>::new([4]));
 
-        let array = Array::new(data.clone(), shape.clone());
-
-        assert_eq!(array.data(), &data);
-        assert_eq!(array.shape().raw_dim().size(), 4);
-        assert_eq!(array.shape().raw_dim().ndim(), 1);
-        assert_eq!(format!("{:?}", array.shape()), format!("{:?}", shape));
+        assert_eq!(data.shape().raw_dim().size(), 4);
+        assert_eq!(data.shape().raw_dim().ndim(), 1);
+        assert_eq!(format!("{:?}", data.shape()), format!("{:?}", shape));
     }
 
     #[test]
     fn test_array_creation_2d() {
-        let data = vec![1, 2, 3, 4, 5, 6]; // TODO: change to arr![] later
-        let shape = Shape::new(Ix::<2>::new([3, 2])); // 3 rows, 2 columns
+        let data = arr![[1, 2], [3, 4], [5, 6]];
+        let shape = Shape::new(Ix::<2>::new([3, 2]));
 
-        let array = Array::new(data.clone(), shape.clone());
-
-        assert_eq!(array.data(), &data);
-        assert_eq!(array.shape().raw_dim().size(), 6);
-        assert_eq!(array.shape().raw_dim().ndim(), 2);
-        assert_eq!(format!("{:?}", array.shape()), format!("{:?}", shape));
+        assert_eq!(data.shape().raw_dim().size(), 6);
+        assert_eq!(data.shape().raw_dim().ndim(), 2);
+        assert_eq!(format!("{:?}", data.shape()), format!("{:?}", shape));
     }
 
-    #[test]
-    fn test_array_creation_3d() {
-        let data = vec![1, 2, 3, 4, 5, 6, 7, 8]; // TODO: change to arr![] later
-        let shape = Shape::new(Ix::<3>::new([2, 2, 2])); // 2 blocks, 2 rows, 2 columns
-
-        let array = Array::new(data.clone(), shape.clone());
-
-        assert_eq!(array.data(), &data);
-        assert_eq!(array.shape().raw_dim().size(), 8);
-        assert_eq!(array.shape().raw_dim().ndim(), 3);
-        assert_eq!(format!("{:?}", array.shape()), format!("{:?}", shape));
-    }
 }
