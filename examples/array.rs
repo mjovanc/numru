49948502f8661b27198919054f48a532bcb0a768
Marcus Cveticanin <mjovanc@icloud.com>
2025-02-08 11:57:29 +0100

    Updating examples

---
+++ examples/array.rs
@@ -0,0 +1,19 @@
+use numru::arr;
+use std::f64::consts::{E, PI, TAU};
+
+fn main() {
+    let a = arr![42, -17, 256, 3, 99, -8];
+    println!("a.shape() = {:?}", a.shape());
+    a.visualize();
+
+    let b = arr![[TAU, -PI, 1.61], [E, 0.98, -7.42], [4.67, -0.45, 8.88]];
+    println!("\nb.shape() = {:?}", b.shape());
+    b.visualize();
+
+    let c = arr![
+        [[101, 202, 303], [404, 505, 606]],
+        [[-707, -808, -909], [111, 222, 333]]
+    ];
+    println!("\nc.shape() = {:?}", c.shape());
+    c.visualize();
+}
--- examples/max.rs
+++ examples/max.rs
@@ -3,35 +3,29 @@ use std::f64::consts::{E, PI, TAU};
 
 fn main() {
     let a = arr![42, -17, 256, 3, 99, -8];
-    println!("1D Shape={:?}", a.shape());
-    a.visualize();
     let a_max = a.max().compute();
-    println!("Max={:?}", a_max);
+    println!("a.max() = {:?}", a_max);
     let a_max = a.max().axis(0).compute();
-    println!("Max Axis=0={:?}", a_max);
+    println!("a.max().axis(0) = {:?}", a_max);
 
     let b = arr![[TAU, -PI, 1.61], [E, 0.98, -7.42], [4.67, -0.45, 8.88]];
-    println!("2D Shape={:?}", b.shape());
-    b.visualize();
     let b_max = b.max().compute();
-    println!("Max={:?}", b_max);
+    println!("b.max() = {:?}", b_max);
     let b_max = b.max().axis(0).compute();
-    println!("Max Axis=0={:?}", b_max);
+    println!("b.max().axis(0) = {:?}", b_max);
     let b_max = b.max().axis(1).compute();
-    println!("Max Axis=1={:?}", b_max);
+    println!("b.max().axis(1) = {:?}", b_max);
 
     let c = arr![
         [[101, 202, 303], [404, 505, 606]],
         [[-707, -808, -909], [111, 222, 333]]
     ];
-    println!("3D Shape={:?}", c.shape());
-    c.visualize();
     let c_max = c.max().compute();
-    println!("Max={:?}", c_max);
+    println!("c.max() = {:?}", c_max);
     let c_max = c.max().axis(0).compute();
-    println!("Max Axis=0={:?}", c_max);
+    println!("c.max().axis(0) = {:?}", c_max);
     let c_max = c.max().axis(1).compute();
-    println!("Max Axis=1={:?}", c_max);
+    println!("c.max().axis(1) = {:?}", c_max);
     let c_max = c.max().axis(2).compute();
-    println!("Max Axis=2={:?}", c_max);
+    println!("c.max().axis(2) = {:?}", c_max);
 }
