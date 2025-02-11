ae7cb49b63d3721fcda577d540602192b73d452b
Marcus Cveticanin <mjovanc@icloud.com>
2025-02-11 10:28:10 +0100

    updating array.rs example to show how visualize works

--- examples/array.rs
+++ examples/array.rs
@@ -4,16 +4,16 @@ use std::f64::consts::{E, PI, TAU};
 fn main() {
     let a = arr![42, -17, 256, 3, 99, -8];
     println!("a.shape() = {:?}", a.shape());
-    a.visualize();
+    a.visualize().execute();
 
     let b = arr![[TAU, -PI, 1.61], [E, 0.98, -7.42], [4.67, -0.45, 8.88]];
     println!("\nb.shape() = {:?}", b.shape());
-    b.visualize();
+    b.visualize().decimal_points(1).execute();
 
     let c = arr![
         [[101, 202, 303], [404, 505, 606]],
         [[-707, -808, -909], [111, 222, 333]]
     ];
     println!("\nc.shape() = {:?}", c.shape());
-    c.visualize();
+    c.visualize().execute();
 }
